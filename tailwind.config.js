c9147ea3b23613cd9c3ba26387c7c7e12fed2d02
tehfinx-lab <tehfinx@gmail.com>
2026-05-12 20:15:19 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        odcBlack: "#050505",
-        odcNavy: "#071723",
-        odcCream: "#F8EBC6",
-        odcRed: "#E51D2A",
-        odcGreen: "#108A50",
-      },
-      boxShadow: {
-        glow: "0 0 35px rgba(229, 29, 42, 0.25)",
-      },
-    },
-  },
-  plugins: [],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./lib/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { colors: { odcBlack: "#050505", odcNavy: "#071723", odcCream: "#F8EBC6", odcRed: "#E51D2A", odcGreen: "#108A50" }, boxShadow: { glow: "0 0 35px rgba(229,29,42,.24)", cream: "0 0 30px rgba(248,235,198,.12)" } } },
+  plugins: []
 };
