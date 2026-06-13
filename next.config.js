69898a28aac8305236e2f93fded58cd775e9e11d
tehfinx-lab <tehfinx@gmail.com>
2026-06-13 23:40:04 +0100

    Add redirects to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/", destination: "/splash", permanent: false },
+    ];
+  },
+};
+
 module.exports = nextConfig;
